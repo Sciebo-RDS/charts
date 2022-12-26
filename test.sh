b2dab8b7654b5a503cd254944fa08d70614cf3e7
jakdimi <47759595+jakdimi@users.noreply.github.com>
2022-12-26 16:36:31 +0100

    added counter for fails

--- test.sh
+++ test.sh
@@ -1,12 +1,54 @@
 BLUE='\033[1;34m'
 NC='\033[0m'
 
+NR_LINT_FAILED=0
+LINT_FAILED=""
+NR_KUBEVAL_FAILED=0
+KUBEVAL_FAILED=""
+
+
 for d in charts/*/ ; do
   echo -e "${BLUE}============================= testing $d =============================${NC}"
   echo -e "${BLUE}----------------------------- building dependencies -----------------------------${NC}"
   helm dependency build "$d"
   echo -e "${BLUE}----------------------------- linting -----------------------------${NC}"
-  helm lint "$d" ""
+  helm lint "$d"
+  
+  if [ $? != 0 ] 
+  then
+    let NR_LINT_FAILED=$NR_LINT_FAILED+1
+    LINT_FAILED="$LINT_FAILED $d"
+  fi
+
   echo -e "${BLUE}----------------------------- kubeval -----------------------------${NC}"
-  helm template "$d" "" | kubeval --ignore-missing-schemas
+  helm template "$d" | kubeval --ignore-missing-schemas
+  
+  if [ $? != 0 ] 
+  then 
+    let NR_KUBEVAL_FAILED=$NR_LINT_FAILED+1
+    KUBEVAL_FAILED="$KUBEVAL_FAILED $d"
+  fi
+
 done
+
+echo
+echo -e "${BLUE}============================= LINT FAILS =============================${NC}"
+echo 
+for d in $LINT_FAILED ; do
+  echo $d
+done
+
+echo
+echo "In total $NR_LINT_FAILED"
+echo
+
+echo
+echo -e "${BLUE}============================= KUBEVAL FAILS =============================${NC}"
+echo 
+for d in $KUBEVAL_FAILED ; do
+  echo $d
+done
+
+echo
+echo "In total $NR_KUBEVAL_FAILED"
+echo
