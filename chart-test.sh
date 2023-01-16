d658154a37849fa24446076e1df83240e604b02e
jakdimi <47759595+jakdimi@users.noreply.github.com>
2023-01-16 12:58:05 +0100

    update chart-test.sh and versions in Chart.lock of all-chart

--- chart-test.sh
+++ chart-test.sh
@@ -1,3 +1,5 @@
+set -e
+
 BLUE='\033[1;34m'
 NC='\033[0m'
 
@@ -10,6 +12,7 @@ KUBEVAL_FAILED=""
 for d in charts/*/ ; do
   echo -e "${BLUE}testing $d ==========================================================${NC}"
   echo -e "${BLUE}building dependencies -----------------------------------------------${NC}"
+  helm dependency update "$d"
   helm dependency build "$d"
   echo -e "${BLUE}linting -------------------------------------------------------------${NC}"
   helm lint "$d"
