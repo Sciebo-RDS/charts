32a73bdda92799f754b542c9c9faf5083adf0992
jakdimi <47759595+jakdimi@users.noreply.github.com>
2023-01-16 13:11:25 +0100

    test all-chart last

--- chart-test.sh
+++ chart-test.sh
@@ -10,28 +10,30 @@ KUBEVAL_FAILED=""
 
 
 for d in charts/*/ ; do
-  echo -e "${BLUE}testing $d ==========================================================${NC}"
-  echo -e "${BLUE}building dependencies -----------------------------------------------${NC}"
-  helm dependency update "$d"
-  helm dependency build "$d"
-  echo -e "${BLUE}linting -------------------------------------------------------------${NC}"
-  helm lint "$d"
-  
-  if [ $? != 0 ] 
+  if [ $d != "charts/all/" ]
   then
-    let NR_LINT_FAILED=$NR_LINT_FAILED+1
-    LINT_FAILED="$LINT_FAILED $d"
-  fi
+    echo -e "${BLUE}testing $d ==========================================================${NC}"
+    echo -e "${BLUE}building dependencies -----------------------------------------------${NC}"
+    helm dependency update "$d"
+    helm dependency build "$d"
+    echo -e "${BLUE}linting -------------------------------------------------------------${NC}"
+    helm lint "$d"
+    
+    if [ $? != 0 ] 
+    then
+      let NR_LINT_FAILED=$NR_LINT_FAILED+1
+      LINT_FAILED="$LINT_FAILED $d"
+    fi
 
-  echo -e "${BLUE}kubeval -------------------------------------------------------------${NC}"
-  helm template "$d" | kubeval --ignore-missing-schemas
-  
-  if [ $? != 0 ] 
-  then 
-    let NR_KUBEVAL_FAILED=$NR_LINT_FAILED+1
-    KUBEVAL_FAILED="$KUBEVAL_FAILED $d"
+    echo -e "${BLUE}kubeval -------------------------------------------------------------${NC}"
+    helm template "$d" | kubeval --ignore-missing-schemas
+    
+    if [ $? != 0 ] 
+    then 
+      let NR_KUBEVAL_FAILED=$NR_LINT_FAILED+1
+      KUBEVAL_FAILED="$KUBEVAL_FAILED $d"
+    fi
   fi
-
 done
 
 echo
