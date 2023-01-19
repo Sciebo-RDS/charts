70317c9200d635ab829ec1e19e1a252eb47bb07a
jakdimi <47759595+jakdimi@users.noreply.github.com>
2023-01-19 19:58:50 +0100

    add helm deploy to gitlab ci - forgit to save

--- ci-scripts/chart-deploy.sh
+++ ci-scripts/chart-deploy.sh
@@ -3,19 +3,24 @@ set -e
 BLUE='\033[1;34m'
 NC='\033[0m'
 
-username="jakdimi"
-password=""
-project="rds/charts"
+username=$HARBOR_ROBOT_NAME
+password=$HARBOR_ROBOT_TOKEN
+project="rds"
 endpoint="https://harbor.uni-muenster.de/api/chartrepo/${project}/charts"
 repo="https://harbor.uni-muenster.de/chartrepo/${project}"
 
 for d in charts/*/ ; do
   export version="$(cat "$d/Chart.yaml" | yq -r .version)"
   export name="$(cat "$d/Chart.yaml" | yq -r .name )"
+  test $name && test $version
   echo Chart 
   echo Name: $name
   echo Version $version
-  test $name && test $version
+  
+  if [ "$test"=="True" ]
+  then
+    version="$version-t"
+  fi
   # Check for uploaded version
   helm repo add --username "$username" --password "$password" "$project" "$repo"
   test $(helm search repo "$project/$name" --version "$version" --output json | jq length) = 0
